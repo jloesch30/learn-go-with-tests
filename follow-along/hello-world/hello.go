5e84414650d5807a0477539561e4ec71c8b1eccb
Josh Loesch <jloesch@apexclearing.com>
2024-11-01 11:25:17 -0500

    Add french and refactor

--- follow-along/hello-world/hello.go
+++ follow-along/hello-world/hello.go
@@ -2,16 +2,36 @@ package main
 
 import "fmt"
 
-const englishHelloPrefix = "Hello, "
+const (
+	english = "English"
+	spanish = "Spanish"
+	french  = "French"
 
-func hello(name string) string {
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+)
+
+func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
 	}
 
-	return englishHelloPrefix + name
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
 
 func main() {
-	fmt.Println(hello("World"))
+	fmt.Println(Hello("World", ""))
 }
--- follow-along/hello-world/hello_test.go
+++ follow-along/hello-world/hello_test.go
@@ -4,19 +4,32 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := hello("Josh")
+		got := Hello("Josh", "")
 		want := "Hello, Josh"
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("saying hello to the world when an empty string is provided", func(t *testing.T) {
-		got := hello("")
+		got := Hello("", "")
 		want := "Hello, World"
 		assertCorrectMessage(t, got, want)
 
 	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Josh", "French")
+		want := "Bonjour, Josh"
+		assertCorrectMessage(t, got, want)
+	})
 }
 
+// we can shorten the syntax of the test function by ommiting the "string" type in the function signature, since they are the same
 func assertCorrectMessage(t testing.TB, got, want string) {
 	// Telling go to treat this function as a helper function, so that when it fails, it will be
 	// reported as a failure on the line number in our test where it was called, rather than inside our helper function.
