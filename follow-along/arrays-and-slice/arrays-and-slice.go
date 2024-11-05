bdb268f217b1ad5dc86825609c2ef02fe602fed7
Josh Loesch <jloesch@apexclearing.com>
2024-11-05 15:02:41 -0600

    Working with slices and arrays

---
+++ follow-along/arrays-and-slice/arrays-and-slice.go
@@ -0,0 +1,12 @@
+package main
+
+func Sum(numbers []int) (sum int) {
+	for _, number := range numbers {
+		sum += number
+	}
+	return
+}
+
+func SumAll(numbersToSum ...[]int) []int {
+	return nil
+}
